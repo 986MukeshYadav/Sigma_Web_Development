99cd1c61e931625a4fddc0231adb90aa856bdda6
Mukesh <myadav986@rku.ac.in>
2024-08-04 17:09:21 +0530

    video 88 Express

---
+++ video88_ExpressJs/main.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.send('Hello world!')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('Hello Contact')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('Hello about')
+})
+
+app.get('/blog',(req,res)=>{
+    res.send('Hello blog')
+})
+
+app.get('/blog/:slug',(req,res)=>{
+     // logic to fetch {slug} from the db
+    // For URL: http://localhost:3000/blog/intro-to-padosi?mode=dark&region=in
+    console.log(req.params) // will output { slug: 'intro-to-padosi' }
+    console.log(req.query) // will output { mode: 'dark', region: 'in' }
+
+    res.send(`hello ${req.params.slug}`)
+})
+
+
+
+// app.get('/blog/intro-to-js', (req, res) => {
+//     // logic to fetch intro to js from the db
+//     res.send('Hello intro-to-js!')
+// })
+
+// app.get('/blog/intro-to-python', (req, res) => {
+//     // logic to fetch intro to python from the db
+//     res.send('Hello intro-to-python!')
+// })
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
