f8231e220e0e24fcbaaed2889b5a36cd81f9246d
Mukesh <myadav986@rku.ac.in>
2024-08-06 17:20:44 +0530

    video92 with ejs Template Engine in Express

---
+++ video92_ejs Template Engine in Express/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs');
+
+// https://github.com/mde/ejs/wiki/Using-EJS-with-Express
+
+app.get('/', (req, res) => {
+    let siteName ="Mukesh"
+    let searchText="Search Now"
+    let arr=["hey", 54 ,65]
+    res.render('index', {siteName:siteName, searchText:searchText, arr})
+})
+
+app.get('/blog/:slug', (req, res) => {
+    let blogTitle ="Adidas why and when?"
+    let blogContent="Its a very good brand"
+    res.render('index', {blogTitle:blogTitle, blogContent:blogContent})
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
