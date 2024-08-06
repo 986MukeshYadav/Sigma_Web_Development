e3833e25f6016b3fe80fec294038ed6144271f40
Mukesh <myadav986@rku.ac.in>
2024-08-06 19:03:50 +0530

    Introduction to JavaScript

---
+++ video 54_IntroductiontoJavaScript/new.js
@@ -0,0 +1,3 @@
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
---
+++ video 54_IntroductiontoJavaScript/script.js
@@ -0,0 +1,19 @@
+alert("Hello world");
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a=prompt("Enter a number")
+var isTrue = confirm("Are you sure want to leave this page")
+
+if(isTrue){
+    console.log("Leaving page...")
+}
+else{
+    console.log("Staying on page...")
+}
+
+console.log("Your number is" +a)
+document.title="Hey I am good"
+
+document.body.style.backgroundColor="red"
